5aaa494fe8c9377db3258929fc5d73514d7153d2
Kantika Telkar <katelkar@cisco.com>
2024-02-29 12:12:14 +0530

    Initial commit

---
+++ App1.java
@@ -0,0 +1,10 @@
+package com.cisco.SimpleMavenProject;
+
+public class App1 {
+	public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the world of the jenkins !" );
+        System.out.println("Simple project for jenkins");
+    }
+}
+
